c94a771f7f29069921118e7bfa3be6e217484ee6
RohitAudit <rohit@persistence.one>
2022-05-04 11:08:09 +0530

    subgraph default 1000 iteration added

--- activate.py
+++ activate.py
@@ -5,25 +5,46 @@ from web3 import Web3
 import argparse
 
 
-class KeysSubgraph:
+class Keysmanager:
     url = None
+    validators = None
 
     def __init__(self, url):
         self.url = url
 
-    def get_validators(self, address: str):
+    def get_validators(self, time, address):
         query = """ {
-  validators(where: {nodeOperator: "address"}) {
-    id
-    nodeOperator
-    publicKey
-    signature
-    status
-  }
-        }
-          """.replace("address", address)
+            validators(
+                first: 1000
+                where: {timestamp_gt: "time_start", nodeOperator: "address"}
+                orderBy: timestamp
+                orderDirection: asc
+            ) {
+                nodeOperator
+                id
+                publicKey
+                signature
+                status
+                timestamp
+                }
+            }
+          """.replace("time_start", str(time)).replace("address", address)
         response = requests.post(self.url, json={'query': query})
-        return response.json()["data"]["validators"]
+        return response.json()
+
+    def get_pubkeys(self, address):
+        """
+        :return: validator keys to monitor , validator keys for validity check
+        """
+        validators = []
+        keys_monitor = self.get_validators(0, address)['data']['validators']
+        while True:
+            validators = validators + keys_monitor
+            if len(keys_monitor) == 1000:
+                keys_monitor = self.get_validators(keys_monitor[1000]["timestamp"], address)['data']['validators']
+            else:
+                break
+        return validators
 
 
 class Issuer:
@@ -49,6 +70,7 @@ class Issuer:
             {'from': self.account.address, 'gasPrice': self.web3_eth.toWei('2', 'gwei'), 'gas': 1000000})
 
     def do_transaction(self, tx):
+
         result = self.web3_eth.eth.call(tx)
         print("=========================result==========================================")
         print(result)
@@ -68,8 +90,9 @@ def main(eth1_endpoint, graph_endpoint, private_key, contract_address, contract_
         a = json.load(file)
     issuer = Issuer(a['abi'], contract_address, eth1_endpoint)
     issuer.set_account(private_key)
-    subgraph = KeysSubgraph(graph_endpoint)
-    validators = subgraph.get_validators(issuer.account.address)
+    subgraph = Keysmanager(graph_endpoint)
+    validators = subgraph.get_pubkeys(issuer.account.address)
+    del issuer
     verified = []
     unverified = []
     deposited = []
@@ -82,22 +105,26 @@ def main(eth1_endpoint, graph_endpoint, private_key, contract_address, contract_
             unverified.append(validator["publicKey"])
     print(
         f"You have {len(unverified)} unverified validators, {len(verified)} verified validators and {len(deposited)} active validators ")
+    exit()
     if len(verified) > 0:
         print("Doing activating keys transaction")
         for key in verified:
+            issuer = Issuer(a['abi'], contract_address, eth1_endpoint)
+            issuer.set_account(private_key)
             if Web3.fromWei(issuer.web3_eth.eth.get_balance(Web3.toChecksumAddress(contract_address)), "ether") > 32:
                 tx = issuer.depositToEth2(key)
                 issuer.do_transaction(tx)
             else:
                 print(
                     f"Balance of pool low for activation. \nCurrent balance: {Web3.fromWei(issuer.web3_eth.eth.get_balance(Web3.toChecksumAddress(contract_address)), 'ether')}")
+                del issuer
                 break
+            del issuer
     else:
         print("No activating deposit pending")
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser("Keys activation script for node operators")
     required = parser.add_argument_group("required arguments")
     required.add_argument("-eth1", "--ethereum-endpoint",
@@ -115,4 +142,4 @@ if __name__ == '__main__':
                         default="contracts/Issuer.json")
     args = parser.parse_args()
     print(args)
-    main(args.ethereum_endpoint,args.graph_endpoint,args.private_key,args.contract_address,args.contract_abi)
+    main(args.ethereum_endpoint, args.graph_endpoint, args.private_key, args.contract_address, args.contract_abi)
